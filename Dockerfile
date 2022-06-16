98225a5d37be08eebf46713fee1f43473d2876dc
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2022-06-16 17:06:42 +0200

    Added HTML and PDF conversion

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM asciidoctor/docker-asciidoctor:1.22.1
 
+WORKDIR /convert
 COPY extract-notes.rb /usr/local/bin/extract-notes.rb
 COPY extract-notes.sh /usr/local/bin/extract-notes
 
--- extract-notes.sh
+++ extract-notes.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ruby /usr/local/bin/extract-notes.rb "$@"
+if [ "$1" = "html" ]; then
+    ruby /usr/local/bin/extract-notes.rb "$2" | asciidoctor - -o ./speaker-notes.html
+elif [ "$1" = "pdf" ]; then
+    ruby /usr/local/bin/extract-notes.rb "$2" | asciidoctor-pdf - -o ./speaker-notes.pdf
+else
+    ruby /usr/local/bin/extract-notes.rb "$1"
+fi
